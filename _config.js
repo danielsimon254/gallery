d3cafc595de0cd5e8411d0b1060c62dcd0189c35
danielsimon254 <daniel1997simon@gmail.com>
2024-11-24 00:18:06 +0300

    edited DB Credentials

--- _config.js
+++ _config.js
@@ -5,7 +5,7 @@ config.mongoURI = {
     //production: 'mongodb+srv://dsimon:learnmoringa2024@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
     //development: 'mongodb+srv://dsimon:learnmoringa2024@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
     //test: 'mongodb+srv://dsimon:learnmoringa2024@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
-   production: 'mongodb+srv://dsimon:learningmoringa2024@cluster-wk3-ip.eim1k.mongodb.net/?retryWrites=true&w=majority&appName=cluster-wk3-ip',
+   production: 'mongodb+srv://dsimon:learnmoringa2024@cluster-wk3-ip.eim1k.mongodb.net/?retryWrites=true&w=majority&appName=cluster-wk3-ip',
 }
 module.exports = config;
 
