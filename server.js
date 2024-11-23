19185d35a5b93654500b1f70230f9040fb09f5d0
danielsimon254 <daniel1997simon@gmail.com>
2024-11-24 02:45:13 +0300

    Added Milestone 3

--- server.js
+++ server.js
@@ -12,21 +12,10 @@ let image = require('./routes/image');
 const app = express();
 
 // connecting the database
-<<<<<<< HEAD
 let mongodb_url = 'mongodb+srv://dsimon:learnmoringa2024@cluster-wk3-ip.eim1k.mongodb.net/';
 let dbName = 'darkroom';
 mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
     if (err) console.log(err)
-=======
-
-const MONGODB_URI = process.env.MONGODB_URI || config.mongoURI[app.settings.env]
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true  },(err)=>{
-    if (err) {
-        console.log(err)
-    }else{
-        console.log(`Connected to Database: ${MONGODB_URI}`)
-    }
->>>>>>> test
 });
 
 // test if the database has connected successfully
@@ -56,14 +45,6 @@ app.use('/image', image);
  
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,() =>{
-<<<<<<< HEAD
     //console.log(`Server is listening at http://localhost:${PORT}`)
     console.log(`Server is listening at https://danielsimon-app-8acad26fbb55.herokuapp.com/:${PORT}`)
 });
-=======
-    console.log(`Server is listening at http://localhost:${PORT}`)
-});
-
-
-module.exports = app;
->>>>>>> test
