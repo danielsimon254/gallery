bc4aff745438dbeb1c2ce009f5aa7265764b9716
danielsimon254 <daniel1997simon@gmail.com>
2024-11-23 22:55:50 +0300

    Alter Configs File

--- _config.js
+++ _config.js
@@ -2,8 +2,11 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://dsimon:learnmoringa2024@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://dsimon:learnmoringa2024@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://dsimon:learnmoringa2024@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    //production: 'mongodb+srv://dsimon:learnmoringa2024@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+    //development: 'mongodb+srv://dsimon:learnmoringa2024@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    //test: 'mongodb+srv://dsimon:learnmoringa2024@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    'mongodb+srv://dsimon:learningmoringa2024@cluster-wk3-ip.eim1k.mongodb.net/?retryWrites=true&w=majority&appName=cluster-wk3-ip'
 }
 module.exports = config;
+
+
